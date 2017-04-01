b2f6f7c97db53947c57cb93f63909ba938610a07
Ahmed Hassan <Ahmed Hassan>
2017-04-01 06:32:33 +0200

    Indexer .exe application is now available

--- Indexer/main.py
+++ Indexer/main.py
@@ -5,16 +5,23 @@
 import Connection as connect
 import Indexer as Index
 import sys
+import os
+
 connection = connect.Connection()
-if __name__ == '__main__':
+clear = lambda: os.system('cls')
+
+if __name__ == '__main__':   
     connection.Start_Connection()
+    clear()
     print("Successfully connected to Database")
+    print("Indexing...")
     
     try:
         while (True):
             Indexer = Index.CreateIndex(connection.cursor)
             connection.db.commit()
     except KeyboardInterrupt:
+        clear()
         print("The Indexer is Exiting")
         connection.Close_Connection()
-        sys.exit(0)  
+        sys.exit(0)
